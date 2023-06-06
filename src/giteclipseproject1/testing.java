903cec7463d290485f0df5048c88a8752f0db6a0
durgi venu"git commit -m "my first commit <venu34892sdv@gmail.com>
2023-06-06 12:16:30 +0530

    testing project1

---
+++ src/giteclipseproject1/testing.java
@@ -0,0 +1,15 @@
+package giteclipseproject1;
+
+public class testing {
+
+	public void fiel1() {
+		System.out.println(" fiel1 = project1");
+		
+	}
+	
+	public static void main(String[] args) {
+		testing t= new testing();
+		t.fiel1();
+	}
+}
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author VENUD
+ *
+ */
+module giteclipseproject1 {
+}
